917898d55b5c3610eef05a3be9c27f31ddef444c
Heilige Geist <heiligegeist01@gmail.com>
2025-08-12 21:45:29 +0000

    after login i was taken back to login screen again

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,7 +11,7 @@ export default async function AdminLayout({
   const user = await getCurrentUser();
 
   if (!user || user.role !== 'admin') {
-    redirect('/login');
+    redirect('/');
   }
 
   return (
