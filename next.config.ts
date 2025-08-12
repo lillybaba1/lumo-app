77fba9b3b60ce4decbc7750ff29eef4133b1aa6a
Heilige Geist <heiligegeist01@gmail.com>
2025-08-12 03:21:01 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: new URL(firebaseConfig.storageBucket).hostname,
+        hostname: firebaseConfig.storageBucket,
       }
     ],
   },
