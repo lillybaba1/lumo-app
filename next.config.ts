35ab9532e79d01b604711caa7ce362e6a484462f
Heilige Geist <heiligegeist01@gmail.com>
2025-08-12 03:19:10 +0000

    still

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: firebaseConfig.storageBucket,
+        hostname: new URL(firebaseConfig.storageBucket).hostname,
       }
     ],
   },
